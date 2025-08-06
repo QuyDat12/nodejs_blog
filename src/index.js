ecfac9a03db26c60f1fdcb15b62c56785310f7bd
QuyDat12 <22t1020055@husc.edu.vn>
2025-08-06 17:46:17 +0700

    Bootstrap4

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express'); // impost thu vien
+const sass = require('sass');
+const path = require('path');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Http logged
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({extname: '.hbs'})); 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
